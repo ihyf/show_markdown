8a23ee8d0b77ad81890a12238a454c2314356e39
huangyingfei <565267598@qq.com>
2020-10-26 10:58:57 +0800

    add test

---
+++ test_hunxiao.py
@@ -0,0 +1,64 @@
+def woshidashabi():
+    print(33333)
+
+
+if __name__ == "__main__":
+
+
+    abc1212121wasasa2121 = "_0x21dd8_0x21dd8_0x21dd822121212_0x21dd8_0x21dd8" + "21212121" + "21212121212"
+    for i in range(0, 1999):
+        abc1212121wasasa2121 += "\x56\x49\x12\x23_0x21dd8rqwed_0x21dd8\u0053\u0074\u0072\u0069\u006e\u0067.\u0066\u0072\u006f\u006d\u0043\u0068\u0061\u0072\u0043\u006f\u0064\u0065"
+    for i in range(0, 100):
+        abc1212121wasasa2121 += "_0x21dd85"
+    _0x21dd8 = "a"
+    print(_0x21dd8)
+
+
+
+    abc1212121wasasa2121 = " "
+    r12121aasadasas12121 = "red"
+    h1212sasas12121 = "Hello"
+    b32321eqwedwqeqe12312 = "bold"
+    wwqweqweqweqweqeqewq = "world"
+    mxyutxwewjshhshqweqwe = __import__('style')
+
+
+
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    eval(f"{abc1212121wasasa2121}(mxyutxwewjshhshqweqwe.{r12121aasadasas12121}('{h1212sasas12121}', mxyutxwewjshhshqweqwe.{b32321eqwedwqeqe12312}('{wwqweqweqweqweqeqewq}') + '!'))")
+    # eval("print(module_t.red('Hello', module_t.yellow('yellow'), module_t.blue('world') + '!'))")
+
+    import style
+    print(style.red('Hello', style.bold('world') + '!'))
+
+
+    import importlib
+
+    m = importlib.import_module("test_case.ihyf_test.tools")
+    eval("m.test_print()")
+
+    eval("woshidashabi()")
+
+
+    """
+    不知道的还以为是份"简历" 真正的名字可能是封"绝笔信"
+
+
+    你用一份所谓正经工作，来掩饰你心中弱
+    你的人生就像container 被些不知名的docker在乱搬
+    日子是一天又那么的一天，就像你手里写下的for循环
+    那么！ 你的梦想是什么？
+    """
+
+
+
+
